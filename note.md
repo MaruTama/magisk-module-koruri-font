c5fa824848691ad187322cb3db1cff035c9d3c23
Yuya Maruyama <18w2094g@shinshu-u.ac.jp>
2020-12-28 15:31:51 +0900

    萌神フォント用に変更した
    miui12 だとうまく行かない？EXT4 dedup filesystem のせい？とりあえずコミットしておく

---
+++ note.md
@@ -0,0 +1,4 @@
+ref
+[OxygenOS 11のフォント「OnePlus Sans」がMagiskモジュールとして移植](https://smartasw.com/archives/oxygenos-11%E3%81%AE%E3%83%95%E3%82%A9%E3%83%B3%E3%83%88%E3%80%8Coneplus-sans%E3%80%8D%E3%81%8Cmagisk%E3%83%A2%E3%82%B8%E3%83%A5%E3%83%BC%E3%83%AB%E3%81%A8%E3%81%97%E3%81%A6%E7%A7%BB%E6%A4%8D.html)
+[[v1.2][FONT] [MAGISK MODULE] OnePlus Sans Systemlessly [Updated v1.2]]
+](https://forum.xda-developers.com/t/v1-2-font-magisk-module-oneplus-sans-systemlessly-updated-v1-2.4148391/)
--- post-fs-data.sh
+++ post-fs-data.sh
@@ -5,47 +5,49 @@
 # This will make sure your module will still work
 # if Magisk change its mount point in the future
 MODDIR=${0%/*}
+SYSETC=$MODDIR/system/etc
+SYSXML=$SYSETC/fonts.xml
 # This script will be executed in post-fs-data mode
 
 #Copy original fonts.xml to the MODDIR to overwrite dummy file
-cp /system/etc/fonts.xml $MODDIR/system/etc
+cp /system/etc/fonts.xml $SYSETC
 
 #Change fonts.xml file
-sed -i 's@<font weight="400" style="normal" index="0">NotoSansCJK-Regular.ttc</font>@<font weight="300" style="normal">Koruri-Light.ttf</font>\n        <font weight="400" style="normal">Koruri-Regular.ttf</font>\n        <font weight="600" style="normal">Koruri-Semibold.ttf</font>\n        <font weight="700" style="normal">Koruri-Bold.ttf</font>\n        <font weight="800" style="normal">Koruri-Extrabold.ttf</font>@g' $MODDIR/system/etc/fonts.xml
+miui_12() {
+	# sed s/置換条件/置換文字/    置換条件を置換文字に変換する。最後にgを付けた場合は置換条件に当てはまるすべての文字列が置換される。gではなくn番目として数字でも指定できる。
+	# sans-serif
+	sed -i 's@<font weight="100" style="normal">Roboto-Thin.ttf</font>@<font weight="100" style="normal">Mali-ExtraLight.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="100" style="italic">Roboto-ThinItalic.ttf</font>@<font weight="100" style="normal">Mali-ExtraLightItalic.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="300" style="normal">Roboto-Light.ttf</font>@<font weight="300" style="normal"Mali-Light.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="300" style="italic">Roboto-LightItalic.ttf</font>@<font weight="300" style="normal">Mali-LightItalic.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="400" style="normal">Roboto-Regular.ttf</font>@<font weight="400" style="normal">Mali-Regular.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="400" style="italic">Roboto-Italic.ttf</font>@<font weight="400" style="normal">Mali-Italic.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="500" style="normal">Roboto-Medium.ttf</font>@<font weight="500" style="normal">Mali-Medium.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="500" style="italic">Roboto-MediumItalic.ttf</font>@<font weight="500" style="normal">Mali-MediumItalic.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="900" style="normal">Roboto-Black.ttf</font>@<font weight="900" style="normal">Mali-SemiBold.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="900" style="italic">Roboto-BlackItalic.ttf</font>@<font weight="900" style="normal">Mali-SemiBoldItalic.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="700" style="normal">Roboto-Bold.ttf</font>@<font weight="700" style="normal">Mali-Bold.ttf</font>@g' $SYSXML
+	sed -i 's@<font weight="700" style="italic">Roboto-BoldItalic.ttf</font>@<font weight="700" style="normal">Mali-BoldItalic.ttf</font>@g' $SYSXML
 
-sed -i 's@<font weight="400" style="normal" index="0" fallbackFor="serif">NotoSerifCJK-Regular.ttc</font>@<font weight="300" style="normal" fallbackFor="serif">Koruri-Light.ttf</font>\n        <font weight="400" style="normal" fallbackFor="serif">Koruri-Regular.ttf</font>\n        <font weight="600" style="normal" fallbackFor="serif">Koruri-Semibold.ttf</font>\n        <font weight="700" style="normal" fallbackFor="serif">Koruri-Bold.ttf</font>\n        <font weight="800" style="normal" fallbackFor="serif">Koruri-Extrabold.ttf</font>@g' $MODDIR/system/etc/fonts.xml
+	# sans-serif-condensed"
+	sed -i 's@<font weight="300" style="normal">RobotoCondensed-Light.ttf</font>@<font weight="300" style="normal">Mali-Light.ttf</font>@g' $SYSXML
+    sed -i 's@<font weight="300" style="italic">RobotoCondensed-LightItalic.ttf</font>@<font weight="300" style="italic">Mali-LightItalic.ttf</font>@g' $SYSXML
+    sed -i 's@<font weight="400" style="normal">RobotoCondensed-Regular.ttf</font>@<font weight="400" style="normal">Mali-Regular.ttf</font>@g' $SYSXML
+    sed -i 's@<font weight="400" style="italic">RobotoCondensed-Italic.ttf</font>@<font weight="400" style="italic">Mali-Italic.ttf</font>@g' $SYSXML
+    sed -i 's@<font weight="500" style="normal">RobotoCondensed-Medium.ttf</font>@<font weight="500" style="normal">Mali-Medium.ttf</font>@g' $SYSXML
+    sed -i 's@<font weight="500" style="italic">RobotoCondensed-MediumItalic.ttf</font>@<font weight="500" style="italic">Mali-MediumItalic.ttf</font>@g' $SYSXML
+    sed -i 's@<font weight="700" style="normal">RobotoCondensed-Bold.ttf</font>@<font weight="700" style="normal">Mali-Bold.ttf</font>@g' $SYSXML
+    sed -i 's@<font weight="700" style="italic">RobotoCondensed-BoldItalic.ttf</font>@<font weight="700" style="italic">Mali-BoldItalic.ttf</font>@g' $SYSXML
 
-#Goodbye, SomcUDGothic
-sed -i 's@SomcUDGothic-Light.ttf@null.ttf@g' $MODDIR/system/etc/fonts.xml
-sed -i 's@SomcUDGothic-Regular.ttf@null.ttf@g' $MODDIR/system/etc/fonts.xml
+	# zh-Hans
+	sed -i '/lang="zh-Hans">/,/family>/s@NotoSansCJK-Regular.ttc@Mengshen-Handwritten.ttf@g' $SYSXML
+	# zh-Hant,zh-Bopo
+	sed -i '/lang="zh-Hant,zh-Bopo">/,/family>/s@NotoSansCJK-Regular.ttc@Mengshen-Handwritten.ttf@g' $SYSXML
+	# ja
+	sed -i '/lang="ja">/,/family>/s@NotoSansCJK-Regular.ttc@Mengshen-Handwritten.ttf@g' $SYSXML
+	# ko
+	sed -i '/lang="ko">/,/family>/s@NotoSansCJK-Regular.ttc@Kosefont-JP.ttf@g' $SYSXML
+}
 
-#Goodbye, OnePlus Font
-sed -i 's@OpFont-@Roboto-@g' $MODDIR/system/etc/fonts.xml
 
-#Copy fonts_slate.xml for OnePlus
-opslate=fonts_slate.xml
-if [ -e /system/etc/$opslate ]; then
-    cp /system/etc/$opslate $MODDIR/system/etc
-	
-	#Change fonts_slate.xml file
-	sed -i 's@<font weight="400" style="normal" index="0">NotoSansCJK-Regular.ttc</font>@<font weight="300" style="normal">Koruri-Light.ttf</font>\n        <font weight="400" style="normal">Koruri-Regular.ttf</font>\n        <font weight="600" style="normal">Koruri-Semibold.ttf</font>\n        <font weight="700" style="normal">Koruri-Bold.ttf</font>\n        <font weight="800" style="normal">Koruri-Extrabold.ttf</font>@g' $MODDIR/system/etc/$opslate
-
-	sed -i 's@SlateForOnePlus-Thin.ttf@Koruri-Light.ttf@g' $MODDIR/system/etc/$opslate
-	sed -i 's@SlateForOnePlus-Light.ttf@Koruri-Light.ttf@g' $MODDIR/system/etc/$opslate
-	sed -i 's@SlateForOnePlus-Book.ttf@Koruri-Regular.ttf@g' $MODDIR/system/etc/$opslate
-	sed -i 's@SlateForOnePlus-Regular.ttf@Koruri-Regular.ttf@g' $MODDIR/system/etc/$opslate
-	sed -i 's@SlateForOnePlus-Medium.ttf@Koruri-Semibold.ttf@g' $MODDIR/system/etc/$opslate
-	sed -i 's@SlateForOnePlus-Bold.ttf@Koruri-Bold.ttf@g' $MODDIR/system/etc/$opslate
-	sed -i 's@SlateForOnePlus-Black.ttf@Koruri-Extrabold.ttf@g' $MODDIR/system/etc/$opslate
-fi
-
-#Copy fonts_base.xml for OnePlus OxygenOS 11+
-oos11=fonts_base.xml
-if [ -e /system/etc/$oos11 ]; then
-    cp /system/etc/$oos11 $MODDIR/system/etc
-	
-	#Change fonts_slate.xml file
-	sed -i 's@<font weight="400" style="normal" index="0">NotoSansCJK-Regular.ttc</font>@<font weight="300" style="normal">Koruri-Light.ttf</font>\n        <font weight="400" style="normal">Koruri-Regular.ttf</font>\n        <font weight="600" style="normal">Koruri-Semibold.ttf</font>\n        <font weight="700" style="normal">Koruri-Bold.ttf</font>\n        <font weight="800" style="normal">Koruri-Extrabold.ttf</font>@g' $MODDIR/system/etc/$oos11
-
-	sed -i 's@<font weight="400" style="normal" index="0" fallbackFor="serif">NotoSerifCJK-Regular.ttc</font>@<font weight="300" style="normal" fallbackFor="serif">Koruri-Light.ttf</font>\n        <font weight="400" style="normal" fallbackFor="serif">Koruri-Regular.ttf</font>\n        <font weight="600" style="normal" fallbackFor="serif">Koruri-Semibold.ttf</font>\n        <font weight="700" style="normal" fallbackFor="serif">Koruri-Bold.ttf</font>\n        <font weight="800" style="normal" fallbackFor="serif">Koruri-Extrabold.ttf</font>@g' $MODDIR/system/etc/$oos11
-fi
+miui_12
